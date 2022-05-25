83b1c5f952d8048f41872fb3beca72cfbdabe833
Ken Fish <fuzionboy64@gmail.com>
2022-05-25 11:40:12 -0400

    a little clean up

--- app/src/main/java/com/revature/App.java
+++ app/src/main/java/com/revature/App.java
@@ -4,11 +4,9 @@
 package com.revature;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        
     }
 }
